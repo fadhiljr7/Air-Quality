20242c8b0c35acefd32abe5f23423e10d702b89b
fadhiljr7 <48880516+fadhiljr7@users.noreply.github.com>
2023-09-29 21:34:36 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -92,11 +92,11 @@ st.write(
 )
 
 # ----- HIDE STREAMLIT STYLE -----
-hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                </style>
-                """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#                 <style>
+#                 #MainMenu {visibility: hidden;}
+#                 footer {visibility: hidden;}
+#                 header {visibility: hidden;}
+#                 </style>
+#                 """
+# st.markdown(hide_st_style, unsafe_allow_html=True)

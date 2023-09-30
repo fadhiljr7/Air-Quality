90c3eb61e69fec5dc46868472f4cb46c5cb39167
fadhiljr7 <48880516+fadhiljr7@users.noreply.github.com>
2023-09-30 10:56:29 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -40,7 +40,7 @@ df_selection = df.query(
 st.title(":bar_chart: Air Quality Dashboard")
 st.markdown("##")
 
-day_count = df_selection.groupby(['year', 'station']).reset_index()
+day_count = df_selection.groupby(['month'])
 average_temp = round(df_selection["TEMP"].mean(), 1)
 average_pres = round(df_selection["PRES"].mean(), 2)
 

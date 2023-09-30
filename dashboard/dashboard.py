e98fa271fc21437f2ca0e778634dac1140c862bc
fadhiljr7 <48880516+fadhiljr7@users.noreply.github.com>
2023-09-30 11:29:09 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -48,8 +48,8 @@ average_pres = round(df_selection["PRES"].mean(), 2)
 left_column, mid_column, mid2_column, right_column = st.columns(4)
 with left_column:
     st.subheader("Days in Total:")
-    #st.subheader(day_count)
-    st.subheader(day_count.count())
+    st.subheader(day_count)
+    #st.subheader(day_count.count())
 with mid_column:
     st.subheader("Average Temp °C:")
     st.subheader(average_temp)
